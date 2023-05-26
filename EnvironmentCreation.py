b7419b57c612853461d04385665925eb63e77acc
KieranM279 <kieranm279@gmail.com>
2023-05-26 23:05:21 +0100

    updating the genome generation

--- EnvironmentCreation.py
+++ EnvironmentCreation.py
@@ -6,7 +6,8 @@ Created on Mon Apr 10 16:54:25 2023
 @author: kieran
 """
 import os
-os.chdir('/Users/kieran/Documents/Symulator')
+#os.chdir('/Users/kieran/Documents/Symulator/')
+os.chdir('/home/kieran/Documents/Symulator/')
 
 import pandas as pd
 import random
@@ -42,6 +43,140 @@ input_args = {}
 
 
 #### Useful functions ####
+
+# Reverses lists and sequences
+def rev(x):
+    
+    # If the input is a string
+    if type(x) == str:
+        new_out = ''
+        
+        # Loop through in reverse
+        for i in range(1,len(x)+1):
+            new_out = new_out + (x[-i])
+        
+    # If the input is a list
+    if type(x) == list:
+        new_out = list()
+        
+        # Loop through in reverse
+        for i in range(1,len(x)+1):
+            new_out.append(x[-i])
+    
+    return(new_out)
+
+# Converts Binary strings to decimals
+def BinDec(string):
+    
+    square_set = [1]
+    output = 0
+    
+    # Generate the set of squares
+    for i in range(1,len(str(string))):
+        
+        square_set.append(2**i)
+    
+    # Reverse the list of squares
+    square_set = rev(square_set)
+    
+    # Generate the final value
+    for d in range(len(string)):
+        output = output + (int(string[d]) * int(square_set[d]))
+    
+    return(output)
+
+# Converts whole integer decimals to binry strings
+def DecBin(x):
+    
+    output = ''
+    
+    while(x>0):
+        
+        # If the number odd
+        if x%2 ==1:
+            
+            x = (x/2)-0.5
+            output = output + '1'
+        
+        else:
+        # If the number is even
+            x = x/2
+            output = output + '0'
+
+    return(rev(output))
+
+# Convert a binary string to a hexadecimal
+def BinHex(x):
+    
+    # Define the conversion dictionary
+    dictionary = {
+        '0000':'0','0001':'1','0010':'2','0011':'3','0100':'4','0101':'5',
+        '0110':'6','0111':'7','1000':'8','1001':'9','1010':'A','1011':'B',
+        '1100':'C','1101':'D','1110':'E','1111':'F'
+        }
+    
+    # Initialise some temporary variables
+    four_bit_list = list()
+    four_bit = ''
+    
+    # Loop through the characters in the string
+    for i in range(len(x)):
+        
+        # Add each character into the four bits
+        four_bit = (four_bit + x[-i-1])
+        
+        # When four bits are together add to a list
+        if len(four_bit) == 4:
+            
+            four_bit_list.append(rev(four_bit))
+            four_bit = ''
+        
+        # Then add the remainder
+        if i == (len(x)-1):
+            
+            four_bit_list.append(rev(four_bit))
+    
+    # Reverse the list
+    four_bit_list = rev(four_bit_list)
+    
+    # Remove my mistakes
+    if four_bit_list[0] == '':
+        four_bit_list.remove('')
+    
+    if len(four_bit_list[0]) < 4:
+        
+        no_z2a = 4-len(four_bit_list[0])
+        zeroes = ''
+        
+        for z in range(no_z2a):
+            zeroes = zeroes + '0'
+    
+        four_bit_list[0] = zeroes + four_bit_list[0]
+    
+    hexadecimal = ''
+    for h in range(len(four_bit_list)):
+        
+        hexadecimal = hexadecimal + dictionary[four_bit_list[h]]
+        
+    return(hexadecimal)
+
+
+# Convert a hexadecimal to a binary string
+def HexBin(x):
+    
+    output = ''
+    dictionary = {
+        '0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101',
+        '6':'0110','7':'0111','8':'1000','9':'1001','A':'1010','B':'1011',
+        'C':'1100','D':'1101','E':'1110','F':'1111'
+        }
+    
+    for i in range(len(x)):
+        output = (output + dictionary[x[i]])
+    
+    return(output)
+
+
 
 # Export 2D Dictionary to '.csv'
 def getArray(dictionary):
@@ -464,66 +599,74 @@ def SOsc(cr_id,change):
     population[cr_id]['Oscillator_period'] = population[cr_id]['Oscillator_period'] + change
 
 
-
+def weightGen():
     
-def GenomeGenerate():
+    type_list = ['0','1']
+    weight = ''
     
-    dictionary = {}
-    
-    
-    input_neurons = ['Age','Osc','TPo','LPo','BdE','BdW','BdN','BdS','NbD','BlE',
-                     'BlW','BlS','BlN']
-    
-    internal_neurons  = list(range(parameters['INTERNAL_NEURONS']))
-    
-    output_neurons = ['MvN','MvE','MvS','MvW','MvR','SOsc']
-    
-    type_list = [0,1]
-    
-    for p in population.keys():
+    for i in range(16):
         
-        entry = {}
+        weight = weight + random.choice(type_list)
         
-        for g in range(parameters['GENES']):
-            
-            source = random.choice(type_list)
-            sink = random.choice(type_list)
-            
-            if source == 0 and sink == 0:
-                
-                entry[g] = {'source_type':0,
-                            'source':random.choice(input_neurons),
-                            'sink_type':0,
-                            'sink':random.choice(internal_neurons),
-                            'weight':round(random.uniform(-4, 4),5)}
-                
-            elif source == 0 and sink == 1:
-                
-                entry[g] = {'source_type':0,
-                            'source':random.choice(input_neurons),
-                            'sink_type':1,
-                            'sink':random.choice(output_neurons),
-                            'weight':round(random.uniform(-4, 4),5)}
-                
-            elif source == 1 and sink == 0:
-                
-                entry[g] = {'source_type':1,
-                            'source':random.choice(internal_neurons),
-                            'sink_type':0,
-                            'sink':random.choice(internal_neurons),
-                            'weight':round(random.uniform(-4, 4),5)}
-            
-            elif source == 1 and sink == 1:
-                
-                entry[g] = {'source_type':1,
-                            'source':random.choice(internal_neurons),
-                            'sink_type':1,
-                            'sink':random.choice(output_neurons),
-                            'weight':round(random.uniform(-4, 4),5)}
-            
-        dictionary[p] = entry
-        
-    return(dictionary)
+    return(weight)
+#def GenomeGenerate():
+#    
+#    dictionary = {}
+#    
+#    
+#    input_neurons = ['Age','Osc','TPo','LPo','BdE','BdW','BdN','BdS','NbD','BlE',
+#                     'BlW','BlS','BlN']
+#    
+#    internal_neurons  = list(range(parameters['INTERNAL_NEURONS']))
+#    
+#    output_neurons = ['MvN','MvE','MvS','MvW','MvR','SOsc']
+#    
+#    type_list = [0,1]
+#    
+#    for p in population.keys():
+#        
+#        entry = {}
+#        
+#        for g in range(parameters['GENES']):
+#            
+#            source = random.choice(type_list)
+#            sink = random.choice(type_list)
+#            
+#            if source == 0 and sink == 0:
+#                
+#                entry[g] = {'source_type':0,
+#                            'source':random.choice(input_neurons),
+#                            'sink_type':0,
+#                            'sink':random.choice(internal_neurons),
+#                            'weight':round(random.uniform(-4, 4),5)}
+#                
+#            elif source == 0 and sink == 1:
+#                
+#                entry[g] = {'source_type':0,
+#                            'source':random.choice(input_neurons),
+#                            'sink_type':1,
+#                            'sink':random.choice(output_neurons),
+#                            'weight':round(random.uniform(-4, 4),5)}
+#                
+#            elif source == 1 and sink == 0:
+#                
+#                entry[g] = {'source_type':1,
+#                            'source':random.choice(internal_neurons),
+#                            'sink_type':0,
+#                            'sink':random.choice(internal_neurons),
+#                            'weight':round(random.uniform(-4, 4),5)}
+#            
+#            elif source == 1 and sink == 1:
+#                
+#                entry[g] = {'source_type':1,
+#                            'source':random.choice(internal_neurons),
+#                            'sink_type':1,
+#                            'sink':random.choice(output_neurons),
+#                            'weight':round(random.uniform(-4, 4),5)}
+#            
+#        dictionary[p] = entry
+#        
+#    return(dictionary)
     
     
 def GenomePrinter(cr_id,df):
@@ -658,7 +801,7 @@ def MainLoop():
 MainLoop()
 
 
-test = GenomeGenerate()
+#test = GenomeGenerate()
 
-GenomePrinter('creature_0', test)
+#GenomePrinter('creature_0', test)
 
