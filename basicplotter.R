71256cc0d235974bb1e0b21f1ee962ba48169647
KieranM297 <kieranm279@gmail.com>
2023-07-30 14:19:48 +0100

    Adding paramters to plot

--- basicplotter.R
+++ basicplotter.R
@@ -8,14 +8,53 @@ library('ggplot2')
 
 
 convert_coordinates <- function(list_of_coords){
-  
+  # Loop through list of coordinates
   for (i in 1:length(list_of_coords)) {
+    # Remove the 
     list_of_coords[i] <- substring(list_of_coords[i],3,nchar(list_of_coords[i]))
   }
   return(list_of_coords)
 }
 
-basic_plotter <- function(filename) {
+
+title_gen <- function(df,tick) {
+  population <- nrow(df)
+  spacer = ""
+  
+  # Enter parameters here
+  generation = 1
+  gene_no = 16
+  int_no = 3
+  grid_size = "128x128"
+  
+  # Processing the parameters
+  title <- "Simulation parameters:"
+  population <- paste("No. organisms = ", population,sep="")
+  generation <- paste("Generation = ",generation,sep="")
+  tick <- paste("Tick = ",tick,sep="")
+  gene_no <- paste("No. genes = ",gene_no,sep="")
+  int_no <- paste("No. internal neurones = ",int_no,sep="")
+  grid_size <- paste("Grid size = ",grid_size,sep="")
+  
+  # Generate final title
+  final_text <- paste(title,
+                      spacer,
+                      population,
+                      generation,
+                      tick,
+                      spacer,
+                      gene_no,
+                      int_no,
+                      spacer,
+                      grid_size,
+                      spacer,
+                      sep = '\n')
+  return(final_text)
+}
+
+
+
+basic_plotter <- function(filename,tick) {
   
   # Import the field
   grid <- read.csv(filename)
@@ -29,6 +68,7 @@ basic_plotter <- function(filename) {
 
   
   df <- df[df$value == 'c',]
+  df[df == "c"] <- "Creatures"
   
   df$Var1 <- as.character((df$Var1))
   df$Var2 <- as.character((df$Var2))
@@ -40,29 +80,35 @@ basic_plotter <- function(filename) {
   df$Var2 <- as.numeric((df$Var2))
   
   
-  
-  plot <- ggplot(df,aes(x=Var2,y=Var1)) +
+  plot <- ggplot(df,aes(x=Var2,y=Var1,fill=value)) +
     geom_point() +
-    theme_bw() +
-    xlim(0,128) +
-    ylim(0,128) +
+    theme_bw(base_size = 15) +
+    xlim(0,127) +
+    ylim(0,127) +
     theme(axis.title = element_blank(),
           axis.text = element_blank(),
           axis.ticks = element_blank(),
-          legend.position = "none")
+          legend.text = element_text(size=15),
+          legend.background = element_rect(fill="lightblue",
+                                     size=0.5, linetype="solid", 
+                                     colour ="darkblue")) +
+    guides(fill=guide_legend(title_gen(df,tick)))
   return(plot)
   
   
 }
 
+
+#print(basic_plotter('0_field.csv',0))
+
 for (i in seq(0,299,1)) {
   print(i)
   
   
-  ggsave(basic_plotter(paste(i,'field.csv',sep="_")),
+  ggsave(basic_plotter(paste(i,'field.csv',sep="_"),i),
          filename = paste('../frames/',i,'_field.png',sep=""),
          width = 10,
-         height = 10,
+         height = 7,
          device = 'png')
   
   
